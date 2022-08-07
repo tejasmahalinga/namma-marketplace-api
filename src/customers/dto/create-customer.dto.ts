d9874f91b5ada8ad17f85dcd6d0e15e24936cd77
tejasmahalinga <tejasmahalinga@gmail.com>
2022-08-07 21:53:09 +0530

    Declared the Customer Entity and createCustomer DTO

--- src/customers/dto/create-customer.dto.ts
+++ src/customers/dto/create-customer.dto.ts
@@ -1,1 +1,11 @@
-export class CreateCustomerDto {}
+import { IsEmail, isPhoneNumber, Max, Min } from "class-validator";
+
+export class CreateCustomerDto {
+  name: string;
+  @IsEmail()
+  email: string;
+  @Min(5000000000)
+  @Max(9999999999)
+  phoneNumber: number;
+  password: string;
+}
--- src/customers/entities/customer.entity.ts
+++ src/customers/entities/customer.entity.ts
@@ -1,1 +1,18 @@
-export class Customer {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Customer {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+  @Column()
+  phoneNumber: number;
+
+  @Column()
+  password: string;
+}
